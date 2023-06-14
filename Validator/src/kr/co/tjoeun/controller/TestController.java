c0e05163fd916932fa71595a3d2c01f1b4a1311d
rnalal <youngk8251@naver.com>
2023-06-14 17:49:17 +0900

    add to test

--- Validator/src/kr/co/tjoeun/controller/TestController.java
+++ Validator/src/kr/co/tjoeun/controller/TestController.java
@@ -32,7 +32,7 @@ public class TestController {
 	@InitBinder
 	protected void initBinder(WebDataBinder binder){
 		TestBean1Validator validator1 = new TestBean1Validator();
-		binder.setValidator(validator1);
-		// binder.setValidators(validator1, validator2, validator3, ...);
+		binder.addValidators(validator1);
+		// binder.addValidators(validator1, validator2, validator3, ...);
 	}	   
 }
